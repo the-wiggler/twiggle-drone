e68163cf42a12c2d60585867da6178c2fd13fee5
twiggle <68298680+the-wiggler@users.noreply.github.com>
2025-06-22 01:12:32 -0400

    added more defined motor control functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include <Arduino.h>
 
-constexpr int MOTOR_FL_PIN  = 2; 	// pin for motor front left
-constexpr int MOTOR_FR_PIN  = 5; 	// pin for motor front right
-constexpr int MOTOR_RL_PIN  = 3; 	// pin for motor rear left
-constexpr int MOTOR_RR_PIN  = 4; 	// pin for motor rear right
+constexpr uint8_t MOTOR_FL_PIN 		= 2; 	// pin for motor front left
+constexpr uint8_t MOTOR_FR_PIN 		= 5; 	// pin for motor front right
+constexpr uint8_t MOTOR_RL_PIN 		= 3; 	// pin for motor rear left
+constexpr uint8_t MOTOR_RR_PIN 		= 4; 	// pin for motor rear right
 
-constexpr int MOTOR_FL = 0; 		// PWM channel for motor front left
-constexpr int MOTOR_FR = 1; 		// PWM channel for motor front right
-constexpr int MOTOR_RL = 2;			// PWM channel for motor front right
-constexpr int MOTOR_RR = 3; 		// PWM channel for motor front right
+constexpr uint8_t MOTOR_FL 			= 0; 	// PWM channel for motor front left
+constexpr uint8_t MOTOR_FR 			= 1; 	// PWM channel for motor front right
+constexpr uint8_t MOTOR_RL 			= 2;	// PWM channel for motor rear left
+constexpr uint8_t MOTOR_RR 			= 3; 	// PWM channel for motor rear right
 
-const int PWM_FREQUENCY = 1000;		// PWM update frequency
-const int PWM_RESOLUTION = 8;		// PWM resolution (bits)
+constexpr uint32_t PWM_FREQUENCY 	= 1000;	// PWM update frequency
+constexpr uint8_t PWM_RESOLUTION 	= 8;	// PWM resolution (bits)
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // SETUP
@@ -42,27 +42,58 @@ void setup() {
 	ledcWrite(MOTOR_RL, 0);
 	ledcWrite(MOTOR_RR, 0);
 
+	Serial.println("Motors initialized.");
 	////////////////////////////////////////////////////////////////////////////////////////////////
 
-	// wait 5 seconds
-	delay(5000);
+	// wait 3 seconds
+	delay(3000);
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // END SETUP
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+uint8_t motorSpeed[4] = { 0, 0, 0, 0 };	// an array that holds the motor speed values to be enacted
+										// on cycle update
+
+// function to change the speeds of all motors by referencing values from motorSpeed
+void updateMotorSpeed() {
+	ledcWrite(MOTOR_FL, motorSpeed[MOTOR_FL]);
+	ledcWrite(MOTOR_FR, motorSpeed[MOTOR_FR]);
+	ledcWrite(MOTOR_RL, motorSpeed[MOTOR_RL]);
+	ledcWrite(MOTOR_RR, motorSpeed[MOTOR_RR]);
+}
+
+// function to change a motors speed that updateMotorSpeed references on cycle update
+void setMotorSpeed(uint8_t motor, uint8_t speed) {
+	if (motor > 3) return;
+	if (speed > 255) speed = 255;	// helps if speed goes over the limit due to bad programming
+	motorSpeed[motor] = speed;
+}
+
+// function that stops all motor movement if there's an emergency
+void emergencyStop() {
+	motorSpeed[MOTOR_FL, 0];
+	motorSpeed[MOTOR_FR, 0];
+	motorSpeed[MOTOR_RL, 0];
+	motorSpeed[MOTOR_RR, 0];
+	updateMotorSpeed();
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // LOOP
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+bool SYSTEM_FAILURE = false;	// this is set to true if the system catches a fatal error that
+								// requires all operations to be halted by (kinda) stopping the loop
 void loop() {
-	for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle += 5) {
-		ledcWrite(MOTOR_RL, dutyCycle);
-		delay(200);
-	}
+	// check if a failure has been detected
+	if (SYSTEM_FAILURE) return;
 
-	ledcWrite(MOTOR_RL, 0);
+	// start cycle
+	setMotorSpeed(MOTOR_FL, 100);
 
-	delay(10000);
+	updateMotorSpeed();
+	// end cycle
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // END LOOP
