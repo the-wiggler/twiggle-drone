6ccac7ff87840c9e8a0b0488339920f76d81aebc
the-wiggler <tyler4077@icloud.com>
2025-06-26 00:55:50 -0400

    updated the core structure
    all non-PID and motor correction tasks will run on core 0 from now on so the motors can update at a constant 200 Hz

--- src/main.cpp
+++ src/main.cpp
@@ -24,64 +24,7 @@ bool SYSTEM_FAILURE = false;				// this is set to true if the system catches a f
 											// error that requires all operations to be halted by 
 											// (kinda) stopping the loop
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// SETUP
-////////////////////////////////////////////////////////////////////////////////////////////////////
-void setup() {
-	Serial.begin(115200);
-	delay(3000); // small delay to allow serial monitor to set up
-
-	/////// MOTOR INITIALIZATION ///////////////////////////////////////////////////////////////////
-	// front left motor
-	ledcSetup(MOTOR_FL, PWM_FREQUENCY, PWM_RESOLUTION);
-	ledcAttachPin(MOTOR_FL_PIN, MOTOR_FL);
-
-	//front right motor
-	ledcSetup(MOTOR_FR, PWM_FREQUENCY, PWM_RESOLUTION);
-	ledcAttachPin(MOTOR_FR_PIN, MOTOR_FR);
-
-	// rear left motor
-	ledcSetup(MOTOR_RL, PWM_FREQUENCY, PWM_RESOLUTION);
-	ledcAttachPin(MOTOR_RL_PIN, MOTOR_RL);
-
-	// rear right motor
-	ledcSetup(MOTOR_RR, PWM_FREQUENCY, PWM_RESOLUTION);
-	ledcAttachPin(MOTOR_RR_PIN, MOTOR_RR);
-
-	// set all motor speeds to 0 at startup
-	ledcWrite(MOTOR_FL, 0);
-	ledcWrite(MOTOR_FR, 0);
-	ledcWrite(MOTOR_RL, 0);
-	ledcWrite(MOTOR_RR, 0);
-
-	Serial.println("Motors initialized.");
-	////////////////////////////////////////////////////////////////////////////////////////////////
-
-	/////// SENSOR INITIALIZATION //////////////////////////////////////////////////////////////////
-	for (int i = 0; i < 11; i++) {
-		if (icm.begin_I2C()) {
-			Serial.println("ICM20948 found!");
-			break;
-		}
-		Serial.println("Failed to connect to ICM20948. Retrying...");
-		delay(1000);
-	}
-	// for some reason it sometimes doesnt connect on the first try...idk why???
-
-	icm.setAccelRateDivisor(0); 	// maximum accelerometer rate
-	icm.setGyroRateDivisor(0); 		// maximum gyroscope rate
-
-	Serial.println("Sensors initialized.");
-	////////////////////////////////////////////////////////////////////////////////////////////////
-
-	// wait 3 seconds
-	Serial.println("Startup...");
-	delay(1000);
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////
-// END SETUP
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
+SemaphoreHandle_t sensorMutex;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // MOTOR FUNCTIONS
@@ -297,32 +240,135 @@ void monitorRollPitchPID(float rollOutput, float pitchOutput) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// LOOP
+// CORE 1 LOOP
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 uint8_t throttle = 200;
+constexpr unsigned long LOOP_INTERVAL = 5000; // 5ms is a 200 Hz delay for the PID system
 
 void loop() {
+	unsigned long  startTime = micros();
+
 	// check if a failure has been detected
 	// if (millis() > 15000) emergencyStop(); // timer to run the program until a cutoff time
 	if (SYSTEM_FAILURE) return;
 
-	// get sensor info
-	readICM();
+	float rollOutput, pitchOutput;
 
-	calculateOrientation();
+	// calculate PID outputs if the sensor data is safe to access (not being used)
+	if (xSemaphoreTake(sensorMutex, 10 / portTICK_PERIOD_MS)) {
+		rollOutput = calculatePID(ROLL_PID, rollErrors, SETPOINT_ROLL, orientations[0]);
+		pitchOutput = calculatePID(PITCH_PID, pitchErrors, SETPOINT_PITCH, orientations[1]);
+		xSemaphoreGive(sensorMutex);
+	}
+	else return; // in case core 0 was using our data :(
 
-	// perform PID calculation
-	float rollOutput = calculatePID(ROLL_PID, rollErrors, SETPOINT_ROLL, orientations[0]);
-	float pitchOutput = calculatePID(PITCH_PID, pitchErrors, SETPOINT_PITCH, orientations[1]);
+	// placeholder since im lazy and havent implemented yaw stuff yet
 	float yawOutput = 0;
 
+	// changes the state of the motor speed arrays to PID corrected values
 	updateMotorsFromPID(rollOutput, pitchOutput, yawOutput, throttle);
 
+	// sends the new motor speeds to the PWM hardware
 	updateMotorSpeed();
 
-	// monitorMotorSpeeds();
 	monitorRollPitchPID(rollOutput, pitchOutput);
+
+	// maintains the 200 Hz clock speed
+	unsigned long elapsedTime = micros() - startTime;
+	if (elapsedTime < LOOP_INTERVAL) delayMicroseconds(LOOP_INTERVAL - elapsedTime); 
+	// apparently delayMicroseconds is a thing!
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// END LOOP
+// END CORE 1
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// CORE 0 LOOP
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void core0Process(void *parameter) {
+	while(true) {
+		if (!SYSTEM_FAILURE) {
+			if (xSemaphoreTake(sensorMutex, portMAX_DELAY)) {
+				readICM();
+				calculateOrientation();
+				xSemaphoreGive(sensorMutex);
+			}
+		}
+		vTaskDelay(10 / portTICK_PERIOD_MS);
+	}
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// END CORE 0
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// SETUP
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void setup() {
+	Serial.begin(115200);
+	delay(3000); // small delay to allow serial monitor to set up
+
+	sensorMutex = xSemaphoreCreateMutex();
+
+	/////// MOTOR INITIALIZATION ///////////////////////////////////////////////////////////////////
+	// front left motor
+	ledcSetup(MOTOR_FL, PWM_FREQUENCY, PWM_RESOLUTION);
+	ledcAttachPin(MOTOR_FL_PIN, MOTOR_FL);
+
+	//front right motor
+	ledcSetup(MOTOR_FR, PWM_FREQUENCY, PWM_RESOLUTION);
+	ledcAttachPin(MOTOR_FR_PIN, MOTOR_FR);
+
+	// rear left motor
+	ledcSetup(MOTOR_RL, PWM_FREQUENCY, PWM_RESOLUTION);
+	ledcAttachPin(MOTOR_RL_PIN, MOTOR_RL);
+
+	// rear right motor
+	ledcSetup(MOTOR_RR, PWM_FREQUENCY, PWM_RESOLUTION);
+	ledcAttachPin(MOTOR_RR_PIN, MOTOR_RR);
+
+	// set all motor speeds to 0 at startup
+	ledcWrite(MOTOR_FL, 0);
+	ledcWrite(MOTOR_FR, 0);
+	ledcWrite(MOTOR_RL, 0);
+	ledcWrite(MOTOR_RR, 0);
+
+	Serial.println("Motors initialized.");
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/////// SENSOR INITIALIZATION //////////////////////////////////////////////////////////////////
+	for (int i = 0; i < 11; i++) {
+		if (icm.begin_I2C()) {
+			Serial.println("ICM20948 found!");
+			break;
+		}
+		Serial.println("Failed to connect to ICM20948. Retrying...");
+		delay(1000);
+	}
+	// for some reason it sometimes doesnt connect on the first try...idk why???
+
+	icm.setAccelRateDivisor(0); 	// maximum accelerometer rate
+	icm.setGyroRateDivisor(0); 		// maximum gyroscope rate
+
+	Serial.println("Sensors initialized.");
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/////// CORE TASK SETUP ////////////////////////////////////////////////////////////////////////
+	xTaskCreatePinnedToCore(
+		core0Process,
+		"core0Process",
+		10000,
+		NULL,
+		2,
+		NULL,
+		0
+	);
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// wait 3 seconds
+	Serial.println("Startup...");
+	delay(1000);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// END SETUP
 ////////////////////////////////////////////////////////////////////////////////////////////////////
