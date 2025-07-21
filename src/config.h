fb781edc3e74a8d7a08f478f9bc8371e19af1388
twiggle <68298680+the-wiggler@users.noreply.github.com>
2025-07-21 13:47:40 -0400

    adjusted PWM_FREQUENCY 10 15khz

--- src/config.h
+++ src/config.h
@@ -13,7 +13,7 @@ constexpr uint8_t MOTOR_FR 			= 1; 	// PWM channel for motor front right
 constexpr uint8_t MOTOR_RL 			= 2;	// PWM channel for motor rear left
 constexpr uint8_t MOTOR_RR 			= 3; 	// PWM channel for motor rear right
 
-constexpr uint32_t PWM_FREQUENCY 	= 78000;	// PWM update frequency
+constexpr uint32_t PWM_FREQUENCY 	= 15000;	// PWM update frequency
 constexpr uint8_t  PWM_RESOLUTION 	= 10;	// PWM resolution (bits)
 
 extern bool SYSTEM_FAILURE;
